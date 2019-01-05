61280ca822d25041afe1e7c69307f5f3d1b7ece0
Bryon <30876973+nukeguard@users.noreply.github.com>
2019-01-04 21:46:05 -0500

    changes to models, thumbnail

--- Data/Scripts/gyro_rotations_inside.cs
+++ Data/Scripts/gyro_rotations_inside.cs
@@ -7,18 +7,18 @@ using VRage.ModAPI;
 using VRage.ObjectBuilders;
 using VRageMath;
 
-namespace nukeguardRotatingGyroSmall
+namespace nukeguardRotatingGyro
 {
-    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_Gyro), false, "LBAnimGyro")]
-    public class RotatingGyroLarge : MyGameLogicComponent
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_Gyro), false, "LBAnimGyroInside")]
+    public class RotatingGyro : MyGameLogicComponent
     {
-        public IMyGyro LGyro_block;
-        public MyEntitySubpart LGyroSubpart;
+        public IMyGyro Gyro_block;
+        public MyEntitySubpart GyroSubpart;
         private float HingePosX = 0f; // Hinge position on the X axis. 0 is center.
         private float HingePosY = -0.051649f; // Hinge position on the Y axis. 0 is center.
         private float HingePosZ = -0.088645f; // Hinge position on the Z axis. 0 is center.
-        private float RotX = 0.01f; // Rotation on the X axis. 0 is no rotation.
-        private float RotY = 0f; // Rotation on the Y axis. 0 is no rotation.
+        private float RotX = 0f; // Rotation on the X axis. 0 is no rotation.
+        private float RotY = 0.18f; // Rotation on the Y axis. 0 is no rotation.
         private float RotZ = 0f; // Rotation on the Z axis. 0 is no rotation.
         public bool InitSubpart = true;
 
@@ -26,12 +26,12 @@ namespace nukeguardRotatingGyroSmall
         private Matrix _rotMatrixX;
         private Matrix _rotMatrixY;
         private Matrix _rotMatrixZ;
-		private Matrix newRotMatrixX;
+		private Matrix newRotMatrixY;
 
         public override void Init(MyObjectBuilder_EntityBase objectBuilder)
         {
             NeedsUpdate |= MyEntityUpdateEnum.EACH_FRAME;
-            LGyro_block = (IMyGyro)Entity;
+            Gyro_block = (IMyGyro)Entity;
 
             // Init the rot matricies here as you shouldnt need to recompute them
             // If you want an angle to be zero, use Matrix.Identity instead :)
@@ -48,28 +48,28 @@ namespace nukeguardRotatingGyroSmall
         {
             try
             {
-                if (!LGyro_block.IsFunctional) return;                // Ignore damaged or build progress blocks.
-                if (LGyro_block.CubeGrid.Physics == null) return;     // Ignore ghost grids (projections).
-				if (LGyro_block.IsWorking) newRotMatrixX = _rotMatrixX;
-                else newRotMatrixX = Matrix.Identity;
+                if (!Gyro_block.IsFunctional) return;                // Ignore damaged or build progress blocks.
+                if (Gyro_block.CubeGrid.Physics == null) return;     // Ignore ghost grids (projections).
+				if (Gyro_block.IsWorking) newRotMatrixY = _rotMatrixY;
+                else newRotMatrixY = Matrix.Identity;
                 if (InitSubpart)
                 {
-                    LGyro_block.TryGetSubpart("AnimGyro", out LGyroSubpart);
+                    Gyro_block.TryGetSubpart("AnimGyroInside", out GyroSubpart);
                     InitSubpart = false;
                 }
 
                 // Checks if subpart is removed (i.e. when changing block color).
-                if (LGyroSubpart.Closed.Equals(true)) ResetLostSubpart();
+                if (GyroSubpart.Closed.Equals(true)) ResetLostSubpart();
 
-                if (LGyroSubpart != null)
+                if (GyroSubpart != null)
                 {
                     var hingePos = new Vector3(HingePosX, HingePosY, HingePosZ); // This defines the location of a new pivot point.
                     var MatrixTransl1 = Matrix.CreateTranslation(-(hingePos));
                     var MatrixTransl2 = Matrix.CreateTranslation(hingePos);
-                    var rotMatrix = LGyroSubpart.PositionComp.LocalMatrix;
+                    var rotMatrix = GyroSubpart.PositionComp.LocalMatrix;
                     // rotMatrix *= (MatrixTransl1 * (Matrix.CreateRotationX(RotX) * Matrix.CreateRotationY(RotY) * Matrix.CreateRotationZ(RotZ)) * MatrixTransl2);
-                    rotMatrix *= (MatrixTransl1 * newRotMatrixX * _rotMatrixY * _rotMatrixZ * MatrixTransl2);
-                    LGyroSubpart.PositionComp.LocalMatrix = rotMatrix;
+                    rotMatrix *= (MatrixTransl1 * newRotMatrixY * _rotMatrixX * _rotMatrixZ * MatrixTransl2);
+                    GyroSubpart.PositionComp.LocalMatrix = rotMatrix;
                 }
             }
             catch (Exception e)
@@ -86,23 +86,23 @@ namespace nukeguardRotatingGyroSmall
         public override void UpdateAfterSimulation()
         {
             if (MyAPIGateway.Utilities.IsDedicated) return;
-            MyEntitySubpart LGyrosubpart;
-            if (LGyro_block != null && LGyro_block.IsWorking && LGyro_block.IsFunctional && LGyro_block.TryGetSubpart("AnimGyro", out LGyrosubpart))
+            MyEntitySubpart Gyrosubpart;
+            if (Gyro_block != null  && Gyro_block.IsWorking && Gyro_block.IsFunctional && Gyro_block.TryGetSubpart("AnimGyroInside", out Gyrosubpart))
             {
-                var _emcolor = LGyro_block.GyroPower;
-                LGyrosubpart.SetEmissiveParts("Emissive", Color.Green, _emcolor);
+                var _emcolor = Gyro_block.GyroPower;
+                Gyrosubpart.SetEmissiveParts("Emissive", Color.Green, _emcolor);
             }
-            else if (LGyro_block != null && LGyro_block.TryGetSubpart("AnimGyro", out LGyrosubpart))
+            else if (Gyro_block != null && Gyro_block.TryGetSubpart("AnimGyroInside", out Gyrosubpart))
             {
-                var _emoff = LGyro_block.GyroPower;
-                LGyrosubpart.SetEmissiveParts("Emissive", Color.Red, _emoff);
+                var _emoff = Gyro_block.GyroPower;
+                Gyrosubpart.SetEmissiveParts("Emissive", Color.Red, _emoff);
             }
         }
 
             private void ResetLostSubpart()
         {
-            LGyroSubpart.Subparts.Clear();
-            LGyro_block.TryGetSubpart("AnimGyro", out LGyroSubpart);
+            GyroSubpart.Subparts.Clear();
+            Gyro_block.TryGetSubpart("AnimGyroInside", out GyroSubpart);
         }
     }
 }
